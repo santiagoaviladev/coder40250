bddc963266b8c6ebea120928e71629129d96304a
Santiago Avila <xavier0823@gmail.com>
2022-06-28 18:00:03 -0500

    Sintaxis

---
+++ Clase01Sintaxis/01Variables/javascript/main.js
@@ -0,0 +1,16 @@
+ 
+  var primerNumero = 200
+  console.log("PRIMER NUMERO", primerNumero)
+
+  let segundoNumero = 300;
+  console.log("SEGUNDO NUMERO", segundoNumero)
+
+  segundoNumero = 400;
+  console.log("SEGUNDO NUMERO MODIFICADO", segundoNumero)
+
+  const tercerNumero = 500;
+  console.log("TERCER NUMERO", tercerNumero)
+
+  /* NO ES VALIDO 
+  tercerNumero = 600;
+  console.log("TERCER NUMERO", tercerNumero) */
---
+++ Clase01Sintaxis/02VariablesConValores/javascript/main.js
@@ -0,0 +1,13 @@
+
+ let miNumero = 1
+ console.log("NUMERO:", miNumero)
+
+ let cadenaDeTexto = "MI CADENA DE TEXTO"
+ console.log("CADENA:", cadenaDeTexto)
+
+ let miBoolean = true //(false)
+ console.log("BOOLEAN:", miBoolean)
+
+ miNumero = "CADENADETEXTO"
+ console.log("NUMERO - CADENADETEXTO:", miNumero)
+
