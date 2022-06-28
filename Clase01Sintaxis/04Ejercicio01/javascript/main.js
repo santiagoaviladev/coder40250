5039e7c3713cdddd3ead7b49bf1428455d093d90
Santiago Avila <xavier0823@gmail.com>
2022-06-28 16:50:21 -0500

    Sintaxis

---
+++ Clase01Sintaxis/04Ejercicio01/javascript/main.js
@@ -0,0 +1,15 @@
+
+ /*
+    Escriba un programa que guard la palabra
+
+    "SUMA"
+   
+    en una variable y los números 3 y 5 en otras variables. 
+
+    Finalmente, imprima en consola el resultado de la operación usando las variables 
+    donde tiene almacenados el 3 y el 5. 
+
+    
+ */
+
+    
---
+++ Clase01Sintaxis/06Ejercicio2/javascript/main.js
@@ -0,0 +1,21 @@
+
+/*
+    Escriba un programa que le pida a un usuario 2 números y muestre:
+
+    A. El resultado de la suma en la consola
+    B. El resultado de la multiplicaión en un alert
+
+
+    Debe indicarle al usuario la operación ejecutada. 
+
+    Por ejemplo, si el usuario ingresa los números 8 y 10
+
+    Debe mostrar un alert que diga: 
+    "La multiplicación de los números ingresados es: 80"
+
+    Y en la consola: 
+    "La suma de los números ingresados es: 18"
+
+
+
+*/
