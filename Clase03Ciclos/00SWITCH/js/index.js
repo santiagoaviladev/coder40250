96beff8aeffe254d1a26a4b53024f5465d91d5b5
Santiago Avila <xavier0823@gmail.com>
2022-07-05 17:21:12 -0500

    switch

--- Clase03Ciclos/00SWITCH/js/index.js
+++ Clase03Ciclos/00SWITCH/js/index.js
@@ -1,30 +1,90 @@
-    /*
-        Escriba un programa que pida al usuario:
-        Una operacion: SUMA / MUL 
-        2 números
-        e imprima en consola, el resultado de la operación.
-        
-        Complete el ejercicio, con DIVISION y RESTA. 
+/*
+    Escriba un programa que pida al usuario:
+    Una operacion: SUMA / MUL 
+    2 números
+    e imprima en consola, el resultado de la operación.
+    
+    Complete el ejercicio, con DIVISION y RESTA. 
 
-        SI TRATA DE DIVIDIR POR 0; MUESTRE UN ERROR
+    SI TRATA DE DIVIDIR POR 0; MUESTRE UN ERROR
 
-    */ 
+*/
 
 
-    const operacion = prompt(`INGRESE UNA OPERACION: 
+const operacion = prompt(`INGRESE UNA OPERACION: 
         SUMA 
         MUL
         DIV
         RESTA`);
 
-    const numero1 = Number(prompt("Ingrese un número"));
-    const numero2 = Number(prompt("Ingrese otro número"));
+const numero1 = Number(prompt("Ingrese un número"));
+const numero2 = Number(prompt("Ingrese otro número"));
 
-    if(isNaN(numero1) || isNaN(numero2))
-    {
-        alert("NUMEROS NO VALIDOS")
+if (isNaN(numero1) || isNaN(numero2)) {
+    alert("NUMEROS NO VALIDOS")
+}
+else {
+    switch (operacion) {
+        case 'SUMA':
+            {
+                const resultado = numero1 + numero2;
+                alert(`El resultado es ${resultado}`)
+                break;
+            }
+        case 'MUL':
+            {
+                const resultado = numero1 * numero2;
+                alert(`El resultado es ${resultado}`)
+                break;
+            }
+        case 'DIV':
+            {
+                const resultado = numero1 / numero2;
+                alert(`El resultado es ${resultado}`)
+                break;
+            }
+        case 'RESTA':
+            {
+                const resultado = numero1 - numero2;
+                alert(`El resultado es ${resultado}`)
+                break;
+            }
+        default:
+            {
+                alert("Operación inválida")
+                break;
+            }
+
     }
-    else
-    {
-        
-    }
+    /* if(operacion === 'SUMA')
+     {
+         const resultado = numero1+numero2;
+         alert(`El resultado es ${resultado}`)
+     }
+     else if(operacion === 'MUL')
+     {
+         const resultado = numero1*numero2;
+         alert(`El resultado es ${resultado}`)
+     }
+     else if(operacion === 'DIV')
+     {
+         if(numero2 === 0){
+             alert("División Inválida")
+         }
+         else 
+         {
+             const resultado = numero1/numero2;
+             alert(`El resultado es ${resultado}`)
+         }
+         
+     }
+     else if(operacion === 'RESTA')
+     {
+         const resultado = numero1-numero2;
+         alert(`El resultado es ${resultado}`)
+     }
+     else 
+     {
+         alert(`La operación es inválida`)
+     } */
+}
